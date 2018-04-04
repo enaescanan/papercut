69f5d3e7b7297568042cd82ace4ea368cd015a1b
enaesccanan <enaescanan@gmail.com>
2018-04-04 15:28:46 +0800

    liters limit

--- js/total.js
+++ js/total.js
@@ -4,21 +4,21 @@ var liters = hashes*13;
 
     $('.litervalue').html(liters+" LITERS");
     
-    if (liters < 600) {
+    if (liters < 601) {
         amount = liters/6;
         $('.litertext').html('flush the toilet '+Math.floor(amount)+' times!');
         $('#result_img').attr("src", "images/flush.png");
 
-    } else if (liters < 1500) {
+    } else if (liters < 1501) {
         amount = liters/30;
         $(".litertext").html('shower for '+Math.floor(amount)+' times! (using a standard shower head, average 3mins per shower)');
         $('#result_img').attr("src", "images/shower.png");
 
-    } else if (liters < 4500) {
+    } else if (liters < 4501) {
         amount = liters/150;
         $(".litertext").html('wash '+Math.floor(amount)+' loads of laundry!');
         $('#result_img').attr("src", "images/laundry.png");
-    } else if (liters > 4500) {
+    } else if (liters > 4501) {
         amount = liters/265;
         $(".litertext").html('fill '+Math.floor(amount)+' bath tubs!');
         $('#result_img').attr("src", "images/tub.png");
